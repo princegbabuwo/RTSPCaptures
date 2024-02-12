c6da9697cbe474c06f7bb7bca2ebb9b713a371c4
toyebrainzgit <toyebrainz@gmail.com>
2024-02-13 00:15:21 +0100

    Tkinter Widgets Are Difined

--- interface/GUI.py
+++ interface/GUI.py
@@ -1,13 +1,13 @@
 from tkinter import *
 from tkinter import ttk
 
-#tkinter._test()
 class Homepage:
     def __init__(self) -> None:
         root = self.__root()
         rootFrame = self.__rootFrame(root)
-        
-        urlFrame = self.__RSTPURLFrame(rootFrame)
+        settings = self.__settings(rootFrame) 
+        urlFrame, urlLabel, urlText = self.__RSTPURLFrame(rootFrame)
+        captureButton = self.__captureButton(rootFrame)
 
         root.mainloop()
 
@@ -21,15 +21,32 @@ class Homepage:
     def __rootFrame(self, parent):
         frame = ttk.Frame(parent)
         frame.grid(column=0, row=0, sticky=(N, W, E, S))
+
+    def __settings(self, parent):
+        button = ttk.Button(parent, text="{-}", command=self.__settingsDialog)
+        button.grid(column=2, row=0, sticky=(N, E))
     
 
     def __RSTPURLFrame(self, parent):
         frame = ttk.Frame(parent)
         frame.grid(column=1, row=0, sticky=(N, W, E))
 
-        label = ttk.Label(frame, text="Enter RTSP URL")
+        label = ttk.Label(frame, text="Enter RTSP URL:")
         label.grid(column=0, row=0, sticky=(N, W))
 
-        text = ttk.Entry
+        text = ttk.Entry(frame)
+        text.grid(column=0, row=1, sticky=(N, W))
 
-        return frame
+        return frame, label, text
+    
+    def __captureButton(self, parent):
+        button = ttk.Button(parent, text="START CAPTURING", command=self.__startCapturing)
+        button.grid(column=1, row=3, sticky=(N, W, E))
+
+        return button
+    
+    def __settingsDialog(self):
+        pass
+
+    def __startCapturing(self):
+        pass
--- main.py
+++ main.py
@@ -1,11 +1,79 @@
 import os, sys, time, traceback
+from turtle import width
 from scripts import RTSP, FTP
 from interface import GUI
+from tkinter import *
+from tkinter import ttk
+
+class Main:
+    def __init__(self) -> None:
+        root = self.__root()
+        rootFrame = self.__rootFrame(root)
+        settings = self.__settings(rootFrame) 
+        urlFrame, urlLabel, urlText = self.__RSTPURLFrame(rootFrame)
+        imgBoxFrame, imgBox = self.__imageBox(rootFrame)
+        captureButton = self.__captureButton(rootFrame)
+        root.mainloop()
+
+    def __root(self):
+        root = Tk()
+        root.title("RTSP Stream Capture")
+        #root.columnconfigure(0, weight=1)
+        #root.rowconfigure(0, weight=1)
+        return root
+
+    def __rootFrame(self, parent):
+        frame = ttk.Frame(parent, height=500)
+        frame.grid(column=0, row=0, sticky=(N, W, E, S))
+
+    def __settings(self, parent):
+        button = ttk.Button(parent, text="{-}", command=self.Events.OpenDialogForSettings)
+        button.grid(column=2, row=0, sticky=(N, E))
+    
+
+    def __RSTPURLFrame(self, parent):
+        frame = ttk.Frame(parent, borderwidth=1, relief='solid')
+        frame.grid(column=1, row=0, sticky=(N, W, E))
+
+        label = ttk.Label(frame, text="Enter RTSP URL:")
+        label.grid(column=0, row=0, sticky=(N, W))
+
+        text = ttk.Entry(frame)
+        text.grid(column=0, row=1, sticky=(N, W))
+
+        return frame, label, text
+    
+    def __imageBox(self, parent):
+        style = ttk.Style()
+        style.configure('Box.TFrame', background='red', padding=5, borderwidth=5, relief='raised')
+        style.configure('Box.TLabel', background='grey', borderwith=2, relief='raised')
+
+        frame = ttk.Frame(parent, width=100, height=100, style="Box.TFrame")
+        frame.grid(column=1, row=1, sticky=(N, W, E, S))
+
+        box = ttk.Label(frame, width=100, style='Box.TLabel')
+        box.grid(column=0, row=0, sticky=(N, S))
+
+        return frame, box
+    
+    def __captureButton(self, parent):
+        button = ttk.Button(parent, text="START CAPTURING", command=self.Events.StartCapture)
+        button.grid(column=1, row=2, sticky=(N, W, E))
+
+        return button
+    
+    class Events:
+        def OpenDialogForSettings(self):
+            pass
+
+        def StartCapture(self):
+            pass
 
 def main():
-    GUI.Homepage()
+    Main()
 
 def script():
+    #return print ("script")
     #define apps inputs/vatiables
     rtsp_url = 'http://camera.buffalotrace.com/mjpg/video.mjpg'
     ftp_server = "ftp.dlptest.com"
